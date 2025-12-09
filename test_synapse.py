93de7dc5c440414120e456923c8f6b3ea35edc8f
Visual Perception and Security Group <155140124+vpsg-research@users.noreply.github.com>
2025-12-09 18:20:02 +0800

    Add files via upload

--- test_synapse.py
+++ test_synapse.py
@@ -40,11 +40,6 @@ parser.add_argument('--seed', type=int, default=2222, help='random seed')
 
 args = parser.parse_args()
 
-if(args.num_classes == 14):
-    classes = ['spleen', 'right kidney', 'left kidney', 'gallbladder', 'esophagus', 'liver', 'stomach', 'aorta', 'inferior vena cava', 'portal vein and splenic vein', 'pancreas', 'right adrenal gland', 'left adrenal gland']
-else:
-    classes = ['spleen', 'right kidney', 'left kidney', 'gallbladder', 'pancreas', 'liver', 'stomach', 'aorta']
-
 def inference(args, model, test_save_path=None):
     db_test = args.Dataset(base_dir=args.volume_path, split="test_vol", list_dir=args.list_dir, nclass=args.num_classes)
     testloader = DataLoader(db_test, batch_size=1, shuffle=False, num_workers=1)
@@ -60,7 +55,7 @@ def inference(args, model, test_save_path=None):
         logging.info('idx %d case %s mean_dice %f mean_hd95 %f, mean_jacard %f mean_asd %f' % (i_batch, case_name, np.mean(metric_i, axis=0)[0], np.mean(metric_i, axis=0)[1], np.mean(metric_i, axis=0)[2], np.mean(metric_i, axis=0)[3]))
     metric_list = metric_list / len(db_test)
     for i in range(1, args.num_classes):
-        logging.info('Mean class (%d) %s mean_dice %f mean_hd95 %f, mean_jacard %f mean_asd %f' % (i, classes[i-1], metric_list[i-1][0], metric_list[i-1][1], metric_list[i-1][2], metric_list[i-1][3]))
+        logging.info('Mean class (%d) mean_dice %f mean_hd95 %f, mean_jacard %f mean_asd %f' % (i, metric_list[i-1][0], metric_list[i-1][1], metric_list[i-1][2], metric_list[i-1][3]))
     performance = np.mean(metric_list, axis=0)[0]
     mean_hd95 = np.mean(metric_list, axis=0)[1]
     mean_jacard = np.mean(metric_list, axis=0)[2]
